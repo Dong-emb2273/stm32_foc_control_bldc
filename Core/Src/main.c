413d85aa9eb40447802cf58601e4499e411bdef9
Dong-dev2273 <dongngoc2003online@gmail.com>
2026-04-28 00:40:17 +0700

    fix fsm and erro nan vel

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -205,29 +205,29 @@ int main(void)
 
 
   /* Sanitize configs in case flash is empty*/
-  // if(E_ZERO==-1){E_ZERO = 0;}
-  // if(M_ZERO==-1){M_ZERO = 0;}
-  // if(isnan(I_BW) || I_BW==-1){I_BW = 1000;}
-  // if(isnan(I_MAX) || I_MAX ==-1){I_MAX=40;}
-  // if(isnan(I_FW_MAX) || I_FW_MAX ==-1){I_FW_MAX=0;}
-  // if(CAN_ID==-1){CAN_ID = 1;}
-  // if(CAN_MASTER==-1){CAN_MASTER = 0;}
-  // if(CAN_TIMEOUT==-1){CAN_TIMEOUT = 1000;}
-  // if(isnan(R_NOMINAL) || R_NOMINAL==-1){R_NOMINAL = 0.0f;}
-  // if(isnan(TEMP_MAX) || TEMP_MAX==-1){TEMP_MAX = 125.0f;}
-  // if(isnan(I_MAX_CONT) || I_MAX_CONT==-1){I_MAX_CONT = 14.0f;}
-  // if(isnan(I_CAL)||I_CAL==-1){I_CAL = 5.0f;}
-  // if(isnan(PPAIRS) || PPAIRS==-1){PPAIRS = 21.0f;}
-  // if(isnan(GR) || GR==-1){GR = 1.0f;}
-  // if(isnan(KT) || KT==-1){KT = 1.0f;}
-  // if(isnan(KP_MAX) || KP_MAX==-1){KP_MAX = 500.0f;}
-  // if(isnan(KP_MIN) || KP_MIN==-1){KP_MIN = 0.0f;}
-  // if(isnan(KD_MAX) || KD_MAX==-1){KD_MAX = 5.0f;}
-  // if(isnan(KD_MIN) || KD_MIN==-1){KD_MIN = 0.0f;}
-  // if(isnan(P_MAX)){P_MAX = 12.5f;}
-  // if(isnan(P_MIN)){P_MIN = -12.5f;}
-  // if(isnan(V_MAX)){V_MAX = 65.0f;}
-  // if(isnan(V_MIN)){V_MIN = -65.0f;}
+  if(E_ZERO==-1){E_ZERO = 0;}
+  if(M_ZERO==-1){M_ZERO = 0;}
+  if(isnan(I_BW) || I_BW==-1){I_BW = 1000;}
+  if(isnan(I_MAX) || I_MAX ==-1){I_MAX=40;}
+  if(isnan(I_FW_MAX) || I_FW_MAX ==-1){I_FW_MAX=0;}
+  if(CAN_ID==-1){CAN_ID = 1;}
+  if(CAN_MASTER==-1){CAN_MASTER = 0;}
+  if(CAN_TIMEOUT==-1){CAN_TIMEOUT = 1000;}
+  if(isnan(R_NOMINAL) || R_NOMINAL==-1){R_NOMINAL = 0.0f;}
+  if(isnan(TEMP_MAX) || TEMP_MAX==-1){TEMP_MAX = 125.0f;}
+  if(isnan(I_MAX_CONT) || I_MAX_CONT==-1){I_MAX_CONT = 14.0f;}
+  if(isnan(I_CAL)||I_CAL==-1){I_CAL = 5.0f;}
+  if(isnan(PPAIRS) || PPAIRS==-1){PPAIRS = 21.0f;}
+  if(isnan(GR) || GR==-1){GR = 1.0f;}
+  if(isnan(KT) || KT==-1){KT = 1.0f;}
+  if(isnan(KP_MAX) || KP_MAX==-1){KP_MAX = 500.0f;}
+  if(isnan(KP_MIN) || KP_MIN==-1){KP_MIN = 0.0f;}
+  if(isnan(KD_MAX) || KD_MAX==-1){KD_MAX = 5.0f;}
+  if(isnan(KD_MIN) || KD_MIN==-1){KD_MIN = 0.0f;}
+  if(isnan(P_MAX)){P_MAX = 12.5f;}
+  if(isnan(P_MIN)){P_MIN = -12.5f;}
+  if(isnan(V_MAX)){V_MAX = 65.0f;}
+  if(isnan(V_MIN)){V_MIN = -65.0f;}
 
   
   // Load Config 
